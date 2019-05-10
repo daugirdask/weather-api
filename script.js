12dc4ad62d412e61e0352f664f7606a2d2b99ed2
Daugirdas <kuprionis@gmail.com>
2019-05-10 20:10:22 +0100

    chart added

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+chart(one=isNaN, two=isNaN);
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
-} else {
-    console.log('not supported;');
-}
-
+} 
 
 function success(position) {
     const latitude = position.coords.latitude;
@@ -14,6 +13,9 @@ function success(position) {
 
         $.getJSON( "https://api.openweathermap.org/data/2.5/" + weather[i] + "?lat=" + latitude + "&lon=" + longitude + "&units=metric" + "&appid=a46b99a6e7611bbef57b3b27b0183785", function( data ) {
             console.log(data);
+            one = 20;
+            two = 5;
+            chart(one, two);
         });
     
     }
@@ -23,3 +25,25 @@ function error() {
     console.log('denied');
 }
 
+function chart(pop) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Temperature',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [one, two]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+    });
+
+}
